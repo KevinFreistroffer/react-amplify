a97d127b0f5eb98857d906809c2543ee9472a5db
Kevin <kevin.freistroffer@gmail.com>
2020-09-15 11:25:13 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Switch, Router } from 'react-router-dom';
+import { Switch, Router, Route } from 'react-router-dom';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <div id="flex-container">
-          <p>paragraph</p>
-          <p>another paragraph</p>
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <Router>
+        <header className="App-header">
+          <div id="flex-container">
+            <p>paragraph</p>
+            <p>another paragraph</p>
+          </div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
+        </header>
+
+        <Switch>
+          <Route path="/view1">View1</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </Router>
     </div>
+
   );
 }
 
