5cd1d31a325eb7526c7b9648f95537cec6cc1ea7
Kevin <kevin.freistroffer@gmail.com>
2020-09-15 11:45:01 -0700

    header padding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function NavBar(props) {
