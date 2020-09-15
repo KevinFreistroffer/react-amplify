38049e305f2c685a67a49c224c165cf763fae0a7
Kevin <kevin.freistroffer@gmail.com>
2020-09-15 10:39:11 -0700

    flex-container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div id="flex-container">
+          <p>paragraph</p>
+          <p>another paragraph</p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
